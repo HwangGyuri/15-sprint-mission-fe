da7b5812606eb1adac7d201566eb850155b1c60c
HwangGyuri <gyuriyuri2580@gmail.com>
2026-08-21 10:02:46 +0900

    임시 파일

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import { getProducts } from './api/product';
+
+// // GET products
+// async function loadProducts() {
+//   const result = await getProducts(1, 10, '');
+//   console.log(result);
+// }
+
+// // loadProducts();
